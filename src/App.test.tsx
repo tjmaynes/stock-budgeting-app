2cbc0748df3a313d0fd198b524f4ce883e744592
TJ Maynes <tj@tjmaynes.com>
2021-10-04 20:52:07 -0400

    Setup initial StockTable

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Stock Budgeting App/i)).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Stock {
+  ticker: string;
+  price: number;
+  percentage: number;
 }
+
+const TopStockRow = () => (
+  <Row>
+    <Col>Stock Ticker</Col>
+    <Col>Price</Col>
+    <Col>Percentage</Col>
+  </Row>
+)
+
+const StockRow: React.FunctionComponent<{stock: Stock}> = ({stock: {ticker, price, percentage}}) => (
+  <Row>
+    <Col>{ticker}</Col>
+    <Col>{price}</Col>
+    <Col>{percentage}</Col>
+  </Row>
+)
+
+const StockTable: React.FunctionComponent<{ stocks: Stock[]}> = ({ stocks }) => (
+  <Container>
+    <TopStockRow />
+    {stocks.map((stock, index) => <StockRow stock={stock} /> )}
+  </Container>
+)
+
+const App = () => (
+  <Container fluid>
+    <header>
+      Stock Budgeting App
+    </header>
+    <StockTable stocks={[
+      { ticker: "TSLA", price: 800.00, percentage: 100.00 }
+    ]} />
+  </Container>
+);
 
 export default App;
